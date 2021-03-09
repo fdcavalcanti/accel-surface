03fe45be66bac02eaa50a1a69c2cc341458a8b5c
fdcavalcanti <filipedoocv@gmail.com>
2021-03-09 14:47:56 -0300

    changed name for MPU address and ACCEL registers

--- Accelerometer_Comm/main.c
+++ Accelerometer_Comm/main.c
@@ -14,31 +14,19 @@
 #define USER_BTN GPIO_PIN_1    // PF1
 #define TIMER_100HZ GPIO_PIN_0 // PD0
 
-#define ACCEL_SDA GPIO_PIN_3 // PB3
-#define ACCEL_SCL GPIO_PIN_2 // PB2
-#define ACCEL_ADDR 0x68      // GY521 Address
-#define WHO_AM_I_ADDR 0x75   // MPU6050's register containing address
+#define MPU_SDA GPIO_PIN_3 // PB3
+#define MPU_SCL GPIO_PIN_2 // PB2
+#define MPU_ADDR 0x68      // GY521 Address
+#define WHO_AM_I_ADDR 0x75   // MPU6050's register containing its address
 #define PWR_MGMT_ADDR 0x6B   // Power Management Register
 
 int8_t data;
-int32_t counter = 0, counter2 = 0;
+unsigned char ACCEL_ADDR[] = {0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40};
 
 void button_interrupt(void);
 int8_t read_byte_I2C0(uint8_t addr);
 void write_byte_I2C0(uint8_t reg, uint8_t data);
-
-void timer100hz_interrupt(void){
-    uint32_t status = TimerIntStatus(TIMER0_BASE, true);
-    if ((status & TIMER_TIMA_TIMEOUT) == TIMER_TIMA_TIMEOUT){
-        GPIOPinWrite(GPIO_PORTF_BASE, LED1, ~GPIOPinRead(GPIO_PORTF_BASE, LED1));
-        data = read_byte_I2C0(WHO_AM_I_ADDR);
-        if (data != 0x68)
-            counter++;
-        else
-            counter2++;
-    }
-    TimerIntClear(TIMER0_BASE, status);
-}
+void timer100hz_interrupt(void);
 
 int main(void)
 {
@@ -73,8 +61,8 @@ int main(void)
     // Accelerometer I2C
     GPIOPinConfigure(GPIO_PB2_I2C0SCL);
     GPIOPinConfigure(GPIO_PB3_I2C0SDA);
-    GPIOPinTypeI2C(GPIO_PORTB_BASE, ACCEL_SDA);
-    GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, ACCEL_SCL);
+    GPIOPinTypeI2C(GPIO_PORTB_BASE, MPU_SDA);
+    GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, MPU_SCL);
     I2CMasterInitExpClk(I2C0_BASE, g_ui32SysClock, false);
     SysCtlDelay(g_ui32SysClock/12000000);
 
@@ -82,17 +70,14 @@ int main(void)
     GPIOPinConfigure(GPIO_PD0_T0CCP0);
     GPIOPinTypeTimer(GPIO_PORTD_BASE, TIMER_100HZ);
     TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-    TimerLoadSet(TIMER0_BASE, TIMER_A, 120000000);  // 100 Hz = clock * periodo
+    TimerLoadSet(TIMER0_BASE, TIMER_A, 1200000);  // 100 Hz = clock * periodo
     TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
     TimerIntRegister(TIMER0_BASE, TIMER_A, timer100hz_interrupt);
     TimerEnable(TIMER0_BASE, TIMER_A);
     TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
-
     write_byte_I2C0(PWR_MGMT_ADDR, 0x00);
-    SysCtlDelay(g_ui32SysClock/12000000);
-    write_byte_I2C0(0x23, 0x88);
-    SysCtlDelay(g_ui32SysClock/12000000);
+    SysCtlDelay(g_ui32SysClock/1200000);
 
     data = read_byte_I2C0(WHO_AM_I_ADDR);
 
@@ -101,6 +86,16 @@ int main(void)
 
 	return 0;
 }
+
+void timer100hz_interrupt(void){
+    uint32_t status = TimerIntStatus(TIMER0_BASE, true);
+    if ((status & TIMER_TIMA_TIMEOUT) == TIMER_TIMA_TIMEOUT){
+        GPIOPinWrite(GPIO_PORTF_BASE, LED1, ~GPIOPinRead(GPIO_PORTF_BASE, LED1));
+        data = read_byte_I2C0(WHO_AM_I_ADDR);
+    }
+    TimerIntClear(TIMER0_BASE, status);
+}
+
 
 void button_interrupt(void){
     uint32_t status = GPIOIntStatus(GPIO_PORTF_BASE, true);
@@ -112,17 +107,18 @@ void button_interrupt(void){
 }
 
 int8_t read_byte_I2C0(uint8_t addr){
-    I2CMasterSlaveAddrSet(I2C0_BASE, ACCEL_ADDR, false);
+    I2CMasterSlaveAddrSet(I2C0_BASE, MPU_ADDR, false);
     I2CMasterDataPut(I2C0_BASE, addr);
     I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
     while(I2CMasterBusy(I2C0_BASE));
 
+    // Need to wait here a bit. Issue with I2CMasterBusy().
     int i = 0;
     do{
         i++;
-    }while(i<100000);
+    }while(i<250);
 
-    I2CMasterSlaveAddrSet(I2C0_BASE, ACCEL_ADDR, true);
+    I2CMasterSlaveAddrSet(I2C0_BASE, MPU_ADDR, true);
     I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
     while(I2CMasterBusy(I2C0_BASE));
 
@@ -134,12 +130,12 @@ int8_t read_byte_I2C0(uint8_t addr){
 
 
 void write_byte_I2C0(uint8_t reg, uint8_t data){
-    I2CMasterSlaveAddrSet(I2C0_BASE, ACCEL_ADDR, false);
+    I2CMasterSlaveAddrSet(I2C0_BASE, MPU_ADDR, false);
     I2CMasterDataPut(I2C0_BASE, reg);
     I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
     while(I2CMasterBusy(I2C0_BASE));
 
-    I2CMasterSlaveAddrSet(I2C0_BASE, ACCEL_ADDR, false);
+    I2CMasterSlaveAddrSet(I2C0_BASE, MPU_ADDR, false);
     I2CMasterDataPut(I2C0_BASE, data);
     I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
     while(I2CMasterBusBusy(I2C0_BASE));
